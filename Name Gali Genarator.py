6ccfd9ebc158532cb77a9d1239cc963f9e479555
KumudTheDude <113181793+KumudTheDude@users.noreply.github.com>
2022-09-12 18:39:18 +0530

    Add files via upload
    Generates 2 random combinations of names with Gali. (DISCLAIMER: Contains abusive language)
    Hit RUN and have FUN

---
+++ Name Gali Genarator.py
@@ -0,0 +1,20 @@
+import os
+import random
+
+name = ["Kumud","Vishal","Sanjana","Saran","Dholu","Ajay", "John", "Mahesh ", "Deepak", "Raja", "Kavya", "White"]
+gali = ["Gay","MILF","Dick Head","lodu","Gandu","Rand", "Horny","HIV+ ","BDSM","pedofile","Legend","Chirand", "eats shit","Pervert","Nigga","Cunt","Macchar ki Jhaant","God"]
+me = ["God","Legend","is epic","Horny",]
+i = 0
+while i < 100:
+    a = random.choice(name)
+    b = random.choice(gali)
+    c = random.choice(me)
+    if a == "Kumud":
+        print(" ")
+        print("                                         .......    ",a, c)
+    else:    
+        print(" ")
+        print("                                         .......    ",a, b)
+        print("                                                         ")
+        print(" ")
+    i = i+50
