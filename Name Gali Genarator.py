55a39f4c4e875f47a2486ed85d50be6f9bad15c7
KumudTheDude <113181793+KumudTheDude@users.noreply.github.com>
2022-09-25 01:53:11 +0530

    Update Name Gali Genarator.py

--- Name Gali Genarator.py
+++ Name Gali Genarator.py
@@ -1,8 +1,8 @@
 import os
 import random
 
-name = ["Kumud","Vishal","Sanjana","Saran","Dholu","Ajay", "John", "Mahesh ", "Deepak", "Raja", "Kavya", "White"]
-gali = ["Gay","MILF","Dick Head","lodu","Gandu","Rand", "Horny","HIV+ ","BDSM","pedofile","Legend","Chirand", "eats shit","Pervert","Nigga","Cunt","Macchar ki Jhaant","God"]
+name = ["Kumud","Vishal","Meghna","Saran","Dholu","Majnu","Ajay", "John", "Mahesh ", "Deepak", "Raja", "Kavya", "Mr.White", "Stuart Little", "Sanjana", "Pakistan","Chacha","Manohar","Vishal"]
+gali = ["Gay","MILF","Dick Head","lodu","Gandu","Rand", "Horny","HIV+ ","BDSM","pedofile","Legend","Chirand", "eats shit","Pervert","Nigga","Cunt","Macchar ki Jhaant","Middle Class","the Rapist","Bitch"]
 me = ["God","Legend","is epic","Horny",]
 i = 0
 while i < 100:
