3a03dfc594b561bdc58aa2321798765033523a2d
KumudTheDude <113181793+KumudTheDude@users.noreply.github.com>
2022-09-25 02:54:09 +0530

    Update Name Gali Genarator.py
    Added Input 
    Added more names and Gali
    Added Heading and lines and other stuff

--- Name Gali Genarator.py
+++ Name Gali Genarator.py
@@ -1,20 +1,40 @@
 import os
 import random
+k=0
+while k<10:
+    print(" ")
+    print(" ")
+    print("...................................................../\/\/\/\/\/\/\/\..................................................... ")
+    print(" ")
+    print(" ")
+    print("                                                  Name & Gali Generator ")
+    print("                                                      -  -  -  -  -        ")
+    print(" ")
+    print(" ")
+    number = int(input("How Many Names Do You Want?:"))
+    print(" ")
+    print(" ")
+    number2 = 100/number
+    name = ["Kumud","Vishal","Meghna","Saran","Dholu","Majnu","Nobita", "John","Baba Ram Dev", "Shaktiman"," Chotha Bheem","Babita Ji","Madhuri","Tarak Meheta","Mahesh", "Deepak", "Raja", "Kavya", "Mr.White", "Stuart Little", "Sanjana", "Pakistan","Champak Chacha","Manohar","Vishal","Mogli","Dumbledore","Phineas and Ferb","Person Reading this is","Kabir","Gabbar","King Louis the IV","King Louis the III","Glenn Quagmire","Ram Aur Lakkhan","Karan Arjun"]
+    gali = ["Gay","MILF","Dick Head","lodu","Gandu","Rand", "Horny","HIV+ ","BDSM","pedofile","Lodu Chand","Chirand", "eats shit","Pervert","Little Bitch","Daily Wage Labour","Nigga","Cunt","Macchar ki Jhaant","Middle Class","the Rapist","Bitch","Gaddhe ki gand","Child Molester","is mentally challanged","is a pain in the ass","Virgin","Dalal","is full of shit"]
+    me = ["God","Legend","is epic","Horny","the best"]
 
-name = ["Kumud","Vishal","Meghna","Saran","Dholu","Majnu","Ajay", "John", "Mahesh ", "Deepak", "Raja", "Kavya", "Mr.White", "Stuart Little", "Sanjana", "Pakistan","Chacha","Manohar","Vishal"]
-gali = ["Gay","MILF","Dick Head","lodu","Gandu","Rand", "Horny","HIV+ ","BDSM","pedofile","Legend","Chirand", "eats shit","Pervert","Nigga","Cunt","Macchar ki Jhaant","Middle Class","the Rapist","Bitch"]
-me = ["God","Legend","is epic","Horny",]
-i = 0
-while i < 100:
-    a = random.choice(name)
-    b = random.choice(gali)
-    c = random.choice(me)
-    if a == "Kumud":
-        print(" ")
-        print("                                         .......    ",a, c)
-    else:    
-        print(" ")
-        print("                                         .......    ",a, b)
-        print("                                                         ")
-        print(" ")
-    i = i+50
+    print(" ")
+    print("  -.-:-.-:-.-:-.-:-.-:-.- ",round(number),"Random Names are Listed Below")
+    print(" ")
+    i = 0
+    while i < 100:
+        a = random.choice(name)
+        b = random.choice(gali)
+        c = random.choice(me)
+        if a == "Kumud":
+            print("                                  .......    ",a, c)
+            print(" ")
+        else:    
+            print(" ")
+            print("                                  .......    ",a, b)
+            print(" ")
+            print(" ")
+        i = i+number2
+    print("                                                                                         Name and Gali Count :", number)
+    k=1
